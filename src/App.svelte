e302b07f7399d9250c87984ef70c22754d5e37bc
Molly Cook Escobar <Molly.CookEscobar@barrons.com>
2025-01-29 17:57:47 -0500

    change last json

--- src/App.svelte
+++ src/App.svelte
@@ -32,7 +32,7 @@
     "https://www.wsj.com/ai2html/d308ed39-2859-41b6-a544-070a4ddd3afb/inset.json",
     "https://www.wsj.com/ai2html/57366dbb-21e7-427c-8318-73cc732b962a/inset.json",
     "https://www.wsj.com/ai2html/3a4bd1f0-7b0f-40b3-ac7b-afc390cbc186/inset.json",
-    "https://www.wsj.com/ai2html/59ba318e-6d9e-4d60-a484-34556d83ff87/inset.json",
+    "https://www.wsj.com/ai2html/1407d854-2433-4373-a43a-2d8ce8748072/inset.json",
   ];
 
   function handleIndexChange(event) {
